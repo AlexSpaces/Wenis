d2d2e801579aabb7c0d585f20c4cef359282c033
Jace Doile <jacedoile@gmail.com>
2023-08-24 16:44:53 -0500

    Added required header

--- index.js
+++ index.js
@@ -47,6 +47,9 @@ const server = http.createServer((req, res) => {
 
         // Reverse Proxy Endpoint
         case '/Travel':
+            res.writeHead(200, {
+                'Access-Control-Allow-Origin': '*',
+            })
             if (req.headers['ToProxyUrl']) {
                 var toProxy = req.headers['ToProxyUrl'];
                 axios.get(toProxy)
