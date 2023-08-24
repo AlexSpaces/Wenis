c1ae964a77ce68ebcf234a712f101c410e4f9261
Jace Doile <jacedoile@gmail.com>
2023-08-24 17:05:50 -0500

    Added required header

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ const server = http.createServer((req, res) => {
     switch (spliturl[0]) {
         // Default Endpoint
         case '/':
+            res.header('Access-Control-Allow-Origin', '*');
             fs.readFile('./Public/index.html', 'utf8', (error, data) => {
                 if (error) {
                     console.log(error);
