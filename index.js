ce364195d279d2bddb81cee240b713b08c34e0cc
Jace Doile <jacedoile@gmail.com>
2023-08-31 20:00:47 -0500

    Fixed issue where theres an extra /

--- index.js
+++ index.js
@@ -12,11 +12,12 @@ function replaceUrls(text, originalURL) {
     return text.replace(urlPattern, (match, pathMatch, httpMatch) => {
         console.log(`====\nMatch: ${match}\nPath Match: ${pathMatch}\nREPLACEMENT: \"/Travel?url=${originalURL}/${pathMatch}\"\nHttp Match: ${httpMatch}\nREPLACEMENT: \"/Travel?url=${httpMatch}\"\n====\n\n`);
         if (pathMatch) {
-            if (pathMatch.slice(1, 1) == '/' || originalURL.splice(-1) == '/') {
-                return `"/Travel?url=${originalURL}${pathMatch}"`;
-            } else {
-                return `"/Travel?url=${originalURL}/${pathMatch}"`;
+            if (pathMatch.slice(1, 1) == '/' && originalURL.splice(-1) ~= '/') {
+                return `"/Travel?url=${originalURL}${pathMatch.slice(1, -1)}"`;
+            } else if (originalURL.splice(-1) == '/' && pathMatch.slice(1, 1) ~= '/') {
+                return `"/Travel?url=${originalURL.slice(1, -1)}${pathMatch}"`;
             };
+            return `"/Travel?url=${originalURL}/${pathMatch}"`;
         } else if (httpMatch) {
             return `"/Travel?url=${httpMatch}"`;
         };
